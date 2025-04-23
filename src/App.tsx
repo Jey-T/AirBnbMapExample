3000a3d65709de338b12c3194301e1c53cad8e93
Jey-T <jeantheo.sambafouala@gmail.com>
2025-04-23 19:10:19 +0200

    Basic google map implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  APIProvider,
+  Map,
+  MapCameraChangedEvent,
+} from "@vis.gl/react-google-maps";
+import { useMemo } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const apiKey = useMemo(() => import.meta.env.VITE_API_KEY, []);
+
+  if (!apiKey) {
+    return <p style={{ color: "red" }}>API key is not set</p>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <APIProvider
+      apiKey={apiKey}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Map
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged={(ev: MapCameraChangedEvent) =>
+            console.log(
+              "camera changed:",
+              ev.detail.center,
+              "zoom:",
+              ev.detail.zoom
+            )
+          }
+        ></Map>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </APIProvider>
+  );
 }
 
-export default App
+export default App;
